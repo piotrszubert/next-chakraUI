7d89a054481d538743cb72ccc1409e2d994096ea
ps777 <56358194+piotrszubert@users.noreply.github.com>
2023-05-23 12:54:32 +0000

    adjusted padding

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,11 +2,11 @@ import { Box, Heading, Text } from '@chakra-ui/react';
 
 const Hero = () => {
   return (
-    <Box bg="blue.100" py={16} px={4} textAlign="center">
+    <Box bg="blue.100" py={60} px={4} textAlign="center">
       <Heading as="h1" size="2xl" mb={4}>
         Welcome to My Website
       </Heading>
-      <Text fontSize="xl" color="whit">
+      <Text fontSize="xl" color="white">
         Placeholder marketing text goes here...
       </Text>
     </Box>
