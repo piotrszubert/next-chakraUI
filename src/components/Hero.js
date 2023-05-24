1a59c1303ed3544d6193adc01769cfe29fcce086
ps777 <56358194+piotrszubert@users.noreply.github.com>
2023-05-24 09:18:34 +0000

    added svg background

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,8 +2,15 @@ import { Box, Heading, Text } from '@chakra-ui/react';
 
 const Hero = () => {
   return (
-    <Box bg="blue.100" py={60} px={4} textAlign="center">
-      <Heading as="h1" size="2xl" mb={4}>
+    <Box
+      bg="black"
+      py={60}
+      px={4}
+      textAlign="center"
+      backgroundImage="url('/hero.svg')"
+      backgroundSize="cover"
+    >
+      <Heading as="h1" size="2xl" color="white" mb={4}>
         Welcome to My Website
       </Heading>
       <Text fontSize="xl" color="white">
