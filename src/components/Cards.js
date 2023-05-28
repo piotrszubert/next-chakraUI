58c353deaa8d261628d66560d25d2d7a1d081a09
ps777 <56358194+piotrszubert@users.noreply.github.com>
2023-05-28 09:57:57 +0200

    renamed component

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -5,14 +5,20 @@ const Features = () => {
 
   const cards = [
     {
+      image: '',
+      date: '',
       heading: 'Best Quality',
       text: 'We care about the quality of the product. As a digital product development agency, we believe in beautiful software.',
     },
     {
+      image: '',
+      date: '',
       heading: 'Modern Tools',
       text: 'We use the modern and most flexible and secure technologies to build the best products on the internet.',
     },
     {
+      image: '',
+      date: '',
       heading: 'Advanced Security',
       text: 'At Software Security Solutions our mission is to raise the bar by making computer security more accessible.',
     },
@@ -42,19 +48,21 @@ const Features = () => {
               borderWidth={2}
               borderColor="brand.500"
               borderRadius="xl"
+              boxShadow='xl'
+              overflow='hidden'
+              transition={'transform .14s cubic-bezier(.25, .46, .45, .69)'}
+              _hover={{
+                cursor: 'pointer',
+                boxShadow: "6px 6px 0px 0px white",
+                transform: "translate(-6px, -6px)"
+              }}
             >
               <Flex
                 direction="column"
                 alignItems="flex-start"
                 p={4}
               >
-                <Icon 
-                  as={HiCheckCircle} 
-                  border='2px solid white' 
-                  borderRadius="full" 
-                  boxSize={10} 
-                  color="brand.600" 
-                />
+                <img src='via.placeholder.com/300'/>
                 <Heading as="h2" size="md"  color="white" mt={4} mb={2}>
                     {card.heading}
                 </Heading>
